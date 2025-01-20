8d9ea494d237a3ff46318025d443b9bab729eadd
jil119 <jilrupani.it22@scet.ac.in>
2025-01-20 09:28:22 +0000

    react_props

---
+++ react_props/src/UserProfile.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function UserProfile({name, age, location, bio}) {
+  return (
+    <div>
+      Name : {name} 
+      <br />
+      Age : {age}
+      <br />
+        Location: {location}
+        <br/>
+        Bio : {bio}
+    </div>
+  )
+}
+
+export default UserProfile
